f858c6c1bd20146648ab1b505b3005d3f1dcab17
Lea Meubrink <lmeubrin@c1b2c1.42berlin.de>
2024-08-27 17:35:27 +0200

    done refactoring

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: lmeubrin <lmeubrin@student.42berlin.       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/04/22 15:55:27 by lmeubrin          #+#    #+#              #
-#    Updated: 2024/08/26 17:39:34 by lmeubrin         ###   ########.fr        #
+#    Updated: 2024/08/27 17:04:05 by lmeubrin         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -29,7 +29,8 @@ OBJ_DIR := obj
 SRCS := main.c newlib.c ft_strtoimax.c ft_sort_int_tab.c operations.c sorting.c\
 		operations2.c op_push.c helpers.c double_linked_list.c sorting_back.c \
 		helpers_print.c targets.c moves.c dlist_helpers.c helpers_sorted.c \
-		moves2.c helpers2.c positional_list.c op_push2.c ft_skip_whitespace.c
+		moves2.c helpers2.c positional_list.c op_push2.c ft_skip_whitespace.c \
+		double_linked_list2.c
 
 OBJS := $(SRCS:%.c=$(OBJ_DIR)/%.o)
 
--- sorting.c
+++ sorting.c
@@ -6,7 +6,7 @@
 /*   By: lmeubrin <lmeubrin@student.42berlin.       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 12:51:30 by lmeubrin          #+#    #+#             */
-/*   Updated: 2024/08/27 15:02:28 by lmeubrin         ###   ########.fr       */
+/*   Updated: 2024/08/27 17:03:02 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,18 +30,9 @@ int	sorting_frame(t_stack *stack_a)
 		sa(&output, stack_a);
 	if (stack_a->len == 3 && !ft_sorted_ascending(stack_a->list, stack_a->len))
 		sort_3_a(&output, stack_a);
-	/* ft_printf("A-----------before sorting back\n"); */
-	/* ft_printf_int_array(stack_a->list, stack_a->len); */
-	/* ft_printf("B-----------before sorting back\n"); */
-	/* ft_printf_int_array(stack_b.list, stack_b.len); */
 	sorting_back(stack_a, &stack_b, &output);
-	/* ft_printf("A-----------after storing back\n"); */
-	/* ft_printf_int_array(stack_a->list, stack_a->len); */
-	/* ft_printf("B-----------after storing back\n"); */
-	/* ft_printf_int_array(stack_b.list, stack_b.len); */
 	if (stack_b.list)
 		free(stack_b.list);
-	/* ft_printf("output after sorting back:\n"); */
 	ft_printf_dlst(&output);
 	ft_dlstclear(&output);
 	return (0);
@@ -92,15 +83,12 @@ void	ft_splitpoint(t_dlist **moves, t_stack *stack_a, t_stack *stack_b)
 	int	splitpoint;
 	int	chunk_size;
 
-	/* splitpoint = 3 / 80 * stack_a->len + 25 / 4; */
 	chunk_size = 19;
 	splitpoint = chunk_size;
 	while (stack_b->len < splitpoint && stack_a->len > 3)
 	{
-        /* if (splitpoint > stack_a->len - 3) */
-        /*     splitpoint = elemnbr - 3; */
-        while (stack_b->len < splitpoint && stack_a->len > 3)
-            ft_handle_operations(moves, stack_a, stack_b, splitpoint + 1);
+		while (stack_b->len < splitpoint && stack_a->len > 3)
+			ft_handle_operations(moves, stack_a, stack_b, splitpoint + 1);
 		splitpoint += chunk_size;
 	}
 }
@@ -112,7 +100,7 @@ int	ft_rot_or_revrot(int *stack, int len, int nb)
 	int	bottom;
 
 	top = 0;
-	while (top < (len / 2) && stack[top] < nb )
+	while (top < (len / 2) && stack[top] < nb)
 		top++;
 	bottom = len - 1;
 	while (bottom >= 0 && stack[bottom] < nb)
